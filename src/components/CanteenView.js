4a629858b827c610bcffdda40f8ddce9fbe40bdf
Martin Otto <paraboloid234@gmail.com>
2020-12-07 11:35:12 +0100

    navigathor: fancier Canteens

--- src/components/CanteenView.js
+++ src/components/CanteenView.js
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {WebView} from 'react-native-webview';
 
 const CanteenView = (props) => {
-      const runFirst =
-      `  $(document).ready(function() {
+  const [showWebView, setShowWebView] = useState(false);
+  const runFirst =
+    `  $(document).ready(function() {
             $("div").css( "display", "none" ); 
             $(".container").css("display", "block"); 
             $(".jedalne").css("display", "block"); 
             $(".col-md-4").css("display", "block");
-            $(".`+props.canteenType+`").eq(`+props.order+`).css("display", "block"); 
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).css("display", "block"); 
             $("h2").css("display", "block");
-            $(".`+props.canteenType+`").eq(`+props.order+`).children().css("display", "block"); 
-            $(".`+props.canteenType+`").eq(`+props.order+`).children().children().css("display", "block");
-            $(".`+props.canteenType+`").eq(`+props.order+`).children().children().children().css("display", "block");
-            $(".`+props.canteenType+`").eq(`+props.order+`).children().children().children().children().css("display", "block");
-            $(".`+props.canteenType+`").eq(`+props.order+`).children().children().children().children().children().css("display", "block");
-            $(".`+props.canteenType+`").eq(`+props.order+`).children().children().children().children().children().children().css("display", "block");
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).children().css("display", "block"); 
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).children().children().css("display", "block");
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).children().children().children().css("display", "block");
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).children().children().children().children().css("display", "block");
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).children().children().children().children().children().css("display", "block");
+            $(".` +
+    props.canteenType +
+    `").eq(` +
+    props.order +
+    `).children().children().children().children().children().children().css("display", "block");
             $("a").css("display", "none"); 
             $(".actualize-info").css("display", "none"); 
    
-            if(`+props.lightMode+` == true) {
+            if(` +
+    props.lightMode +
+    ` == true) {
                $("h2").css("color", "rgb(0,0,0)");
                $("h2").css("border-color", "rgb(255,215,0)");
                $(".content").css("border-color", "rgb(255,215,0)");
-               $(".`+props.canteenType+`").css("border-color", "rgb(255,215,0)");
-               $(".`+props.canteenType+`").css("background-color", "rgb(255,255,255)");
+               $(".` +
+    props.canteenType +
+    `").css("border-color", "rgb(255,215,0)");
+               $(".` +
+    props.canteenType +
+    `").css("background-color", "rgb(255,255,255)");
                $(".header.col-xs-12").css("color", "rgb(0,0,0)");
                $(".has_popup").css("color", "rgb(0,0,0)");
                $(".announcement").css("background-color", "rgb(255,215,0)");
@@ -41,8 +76,12 @@ const CanteenView = (props) => {
                $("h2").css("color", "rgb(255,215,0)");
                $("h2").css("border-color", "rgb(255,215,0)");
                $(".content").css("border-color", "rgb(255,215,0)");
-               $(".`+props.canteenType+`").css("border-color", "rgb(255,215,0)");
-               $(".`+props.canteenType+`").css("background-color", "rgb(0,0,0)");
+               $(".` +
+    props.canteenType +
+    `").css("border-color", "rgb(255,215,0)");
+               $(".` +
+    props.canteenType +
+    `").css("background-color", "rgb(0,0,0)");
                $(".header.col-xs-12").css("color", "rgb(255,215,0)");
                $(".has_popup").css("color", "rgb(255,255,255)");
                $(".announcement").css("background-color", "rgb(255,215,0)");
@@ -59,10 +98,27 @@ const CanteenView = (props) => {
       `;
   return (
     <WebView
-      style={{position:'relative', marginTop: -90}}
+      style={
+        showWebView
+          ? {
+              backgroundColor: 'transparent',
+              position: 'relative',
+              marginTop: -90,
+              flex: 1,
+            }
+          : {backgroundColor: 'transparent', display: 'none'}
+      }
       source={{uri: 'http://jedalen.tuke.sk'}}
       onMessage={(event) => {}}
       injectedJavaScript={runFirst}
+      onLoadStart={() => {
+        setShowWebView(false);
+      }}
+      onLoadEnd={() => {
+        if (!showWebView) {
+          setShowWebView(true);
+        }
+      }}
     />
   );
 };
